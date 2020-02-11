5d792c8214317cec829b9ae8372baffca19545b8
HB <hendrik.bomhardt@student.hpi.de>
2020-02-11 17:41:18 +0100

    Change modid name

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -43,8 +43,8 @@ import java.util.function.Function;
 
 @Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION, clientSideOnly = true)
 public class ExampleMod {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "searchBotWithNotifications";
+    public static final String NAME = "Search Bot with Telegram Notifications";
     public static final String VERSION = "1.0";
 
     private static final Logger LOGGER = LogManager.getLogger();
@@ -129,7 +129,7 @@ public class ExampleMod {
         blocksToSearchFor.add("end_portal_frame");
 
 
-        lithium = new TelegramBot("***REMOVED***");
+        lithium = new TelegramBot("YOUR TELEGRAM BOT API KEY HERE");
         lithium.setUpdatesListener(updates -> {
             for (Update update : updates) {
 
